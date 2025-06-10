30a59a3668865b992d0068e373d0940185d0a212
RTK <142771358+singhtarun10@users.noreply.github.com>
2025-06-10 19:37:15 +0530

    versions.tf

--- task03/versions.tf
+++ task03/versions.tf
@@ -12,9 +12,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "057e5d87-38b5-4af8-88a6-******c69a2d"
-  tenant_id       = "b41b72d0-4e9f-4c26-8a69-f949******7c91d"
-  client_id       = "9f3322b1-3fa4-40bc-b3cc*********2e1c"
-  client_secret   = "6AE8Q~64jU6XYUuBcT*&&&&*******UVr~7cuV"
-
 }
